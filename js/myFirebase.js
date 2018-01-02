f50cf4e5c334d880b18645b4781f9573bfd1bdb0
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-02 12:22:01 -0300

    First Commit

---
+++ js/myFirebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCard9eQg9ssp1MiL-CLFrRC4YEy5Xd1As",
+    authDomain: "fickeroo.firebaseapp.com",
+    databaseURL: "https://fickeroo.firebaseio.com",
+    projectId: "fickeroo",
+    storageBucket: "",
+    messagingSenderId: "824926587604"
+  };
+  firebase.initializeApp(config);
