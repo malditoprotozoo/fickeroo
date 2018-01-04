5a2d2b19af9071f19d3ca2a08835d0434f49d991
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-04 16:44:41 -0300

    Firebase added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,18 @@
-var 
+// Inicializa Firebase
+var config = {
+  apiKey: "AIzaSyCard9eQg9ssp1MiL-CLFrRC4YEy5Xd1As",
+  authDomain: "fickeroo.firebaseapp.com",
+  databaseURL: "https://fickeroo.firebaseio.com",
+  projectId: "fickeroo",
+  storageBucket: "fickeroo.appspot.com",
+  messagingSenderId: "824926587604"
+};
+
+var loginName = $("#login-username");
+var loginPass = $("#login-pass");
 var loginBtn = $(".loginmodal-submit");
 
 
 $(document).ready(function() {
-  
+  firebase.initializeApp(config);
 });
